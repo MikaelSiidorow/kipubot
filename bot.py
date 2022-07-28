69d1999af0bb2957a1fdfbd441c4716f1f66435c
Mikael Siidorow <mikael.siidorow@aalto.fi>
2022-07-29 02:08:42 +0300

    Implement conversation for setting up a raffle

--- bot.py
+++ bot.py
@@ -2,15 +2,20 @@ import os
 import logging
 import sqlite3
 import sys
+from typing import Union
 import matplotlib.pyplot as plt
 import pandas as pd
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ChatMemberStatus, MessageEntityType
-from telegram.ext import ApplicationBuilder, CommandHandler, \
-    ContextTypes, MessageHandler, ChatMemberHandler, CallbackQueryHandler
+from telegram.ext import (ApplicationBuilder, CommandHandler,
+                          ContextTypes, MessageHandler, ChatMemberHandler,
+                          CallbackQueryHandler, ConversationHandler,
+                          PicklePersistence)
 from telegram.ext.filters import Document
 import telegram.ext.filters as Filters
+from telegram._files.document import Document as DocumentFile
+
 
 # -- SETUP --
 logging.basicConfig(
@@ -21,6 +26,7 @@ logging.basicConfig(
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN', default=None)
+EXCEL_MIME = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
 if BOT_TOKEN is None:
     print('Bot token not found')
@@ -36,10 +42,9 @@ print('Connected!')
 # -- FUNCTIONS --
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!")
+async def start(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Use the given commands or send me an Excel-file' +
+                                    ' from MobilePay if you\'re the host of a raffle!')
 
 
 async def hello(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -49,7 +54,7 @@ async def hello(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
     chat = update.effective_chat.title
 
     try:
-        cur.execute(f'''INSERT OR IGNORE INTO users(user_id, username)
+        cur.execute(f'''INSERT OR IGNORE INTO user(user_id, username)
                         VALUES ({user_id}, '{username}')''')
 
         try:
@@ -64,59 +69,255 @@ async def hello(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
     con.commit()
 
 
-async def excel_file(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
+async def excel_file(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
     user_id = update.effective_user.id
 
-    query_result = \
-        cur.execute(f'''SELECT c.chat_id, c.title
-                        FROM chats AS c, in_chat AS i
-                        WHERE i.user_id = {user_id} AND c.chat_id = i.chat_id''') \
-        .fetchall()
+    query_result = (cur.execute(f'''SELECT c.chat_id, c.title
+                        FROM chat AS c, in_chat AS i
+                        WHERE i.user_id = {user_id} AND c.chat_id = i.chat_id
+                            AND(c.cur_winner = {user_id} OR
+                                c.prev_winner = {user_id} OR
+                                c.admin = {user_id})''')
+                    .fetchall())
 
     if len(query_result) == 0:
-        await update.message.reply_text('You are not the winner in any chat!')
+        await update.message.reply_text('You are not the winner in any chat! ❌')
+        return ConversationHandler.END
 
-    else:
-        doc = update.message.document
+    doc = update.message.document
 
-        chat_buttons = []
+    chat_buttons = []
 
-        for chat_id, chat_title in query_result:
-            chat_buttons.append(InlineKeyboardButton(
-                chat_title, callback_data=[chat_id, chat_title, doc]))
+    for chat_id, chat_title in query_result:
+        chat_buttons.append(InlineKeyboardButton(
+            '💬 ' + chat_title, callback_data=[chat_id, chat_title, doc]))
 
+    keyboard = [
+        chat_buttons,
+        [InlineKeyboardButton('❌ Cancel', callback_data='cancel')]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        'Choose channel:', reply_markup=reply_markup)
+
+
+async def setup_raffle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
+    query = update.callback_query
+    # print('setup_raffle: ' + str(update))
+
+    if query.data == 'cancel':
+        await query.message.edit_text('Cancelled! ❌')
+        context.user_data.clear()
+        return ConversationHandler.END
+
+    if (len(query.data) != 3 or not isinstance(query.data[0], int) or
+            not isinstance(query.data[1], str) or
+            not isinstance(query.data[2], DocumentFile)):
+        await query.message.edit_text('Unknown error, please try again later! ❌')
+        return ConversationHandler.END
+
+    chat_id = query.data[0]
+    chat_title = query.data[1]
+    file = await context.bot.get_file(query.data[2])
+
+    context.user_data['raffle_chat_id'] = chat_id
+    context.user_data['raffle_chat_title'] = chat_title
+
+    if not os.path.exists(f'data/{chat_id}'):
+        os.mkdir(f'data/{chat_id}')
+
+    with open(f'data/{chat_id}/data.xlsx', 'wb') as f:
+        await file.download(out=f)
+
+    raffle_data = (cur.execute(f'''SELECT *
+                        FROM raffle
+                        WHERE chat_id = {chat_id}''')
+                   .fetchone())
+
+    if raffle_data is not None:
         keyboard = [
-            chat_buttons,
-            [InlineKeyboardButton('Cancel', callback_data='cancel_update')]
+            [InlineKeyboardButton(
+                '🆕 Create a new raffle!', callback_data='raffle:new_raffle')],
+            [InlineKeyboardButton(
+                '🔄 Update existing raffle!', callback_data='raffle:use_existing')],
+            [InlineKeyboardButton(
+                '❌ Cancel!', callback_data='cancel')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await query.message.edit_text(f'Selected {chat_title}!\n' +
+                                      'Found existing raffle.\n' +
+                                      'Do you want update it or create a new one?')
+
+        await query.message.edit_reply_markup(reply_markup)
+    else:
+        keyboard = [
+            [InlineKeyboardButton(
+                '🆕 Create a new raffle!', callback_data='raffle:new_raffle')],
+            [InlineKeyboardButton(
+                '❌ Cancel!', callback_data='cancel')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await query.message.edit_text(f'Selected {chat_title}! \n' +
+                                      'No existing raffle found. ' +
+                                      'Do you want to create a new one?')
+
+        await query.message.edit_reply_markup(reply_markup)
+
+    return 'ask_date'
+
+
+async def ask_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
+    query = update.callback_query
+    # print('ask_date: ' + str(update))
+    chat_title = context.user_data['raffle_chat_title']
+
+    if query.data == 'cancel':
+        await query.message.edit_text('Cancelled! ❌')
+        context.user_data.clear()
+        return ConversationHandler.END
+
+    command = query.data.split(':')[1]
+
+    if command == 'use_existing':
+        await query.message.edit_text(f'Updated raffle data in {chat_title}! 🔄')
+        context.user_data.clear()
+        return ConversationHandler.END
+
+    await query.message.edit_text('Send the start and end date of the raffle!\n\n' +
+                                  'Format (start and end date on separate lines): \n' +
+                                  'YYYY-MM-DD HH:MM\n' +
+                                  'YYYY-MM-DD HH:MM\n\n' +
+                                  '/cancel to cancel')
+
+    return 'get_date'
+
+
+async def invalid_date(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
+    # print('invalid_date: ' + str(update))
+
+    await update.message.reply_text('Invalid date! ❌')
+
+    return 'get_date'
+
+
+async def get_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    # print('get_date: ' + str(update))
+
+    start_date, end_date = update.message.text.split("\n")
+
+    context.user_data['raffle_start_date'] = start_date
+    context.user_data['raffle_end_date'] = end_date
+
+    keyboard = [
+        [InlineKeyboardButton('#️⃣ Set fee', callback_data='fee:continue')],
+        [InlineKeyboardButton('1️⃣ Use default (1€)',
+                              callback_data='fee:default')],
+        [InlineKeyboardButton('❌ Cancel', callback_data='cancel')]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(f'Start date set to {start_date}!\n' +
+                                    f'End date set to {end_date}!', reply_markup=reply_markup)
+
+    return 'ask_fee'
+
+
+async def ask_fee(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
+    # print('ask_fee: ' + str(update))
+    query = update.callback_query
+
+    if query.data == 'cancel':
+        await query.message.edit_text('Cancelled! ❌')
+        context.user_data.clear()
+        return ConversationHandler.END
+
+    if query.data == 'fee:default':
+        context.user_data['raffle_entry_fee'] = 100
+        keyboard = [
+            [InlineKeyboardButton('✔️ Finish raffle', callback_data='finish')],
+            [InlineKeyboardButton('❌ Cancel', callback_data='cancel')]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
-        await update.message.reply_text(
-            'Choose channel:', reply_markup=reply_markup)
+        await query.message.edit_text('Fee set to 1€!', reply_markup=reply_markup)
+
+        return 'finish_setup'
+
+    await query.message.edit_text('Send the entry fee to the raffle!\n\n' +
+                                  'Example inputs: \n' +
+                                  '0.50, 1.5, 2\n\n' +
+                                  '/cancel to cancel')
+
+    return 'get_fee'
 
 
-async def update_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def invalid_fee(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> str:
+    await update.message.reply_text('Invalid fee! ❌')
+
+    # print('invalid_fee: ' + str(update))
+
+    return 'get_fee'
+
+
+async def get_fee(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    # print('get_fee: ' + str(update))
+
+    fee = update.message.text
+    context.user_data['raffle_entry_fee'] = int(float(fee) * 100)
+
+    keyboard = [
+        [InlineKeyboardButton('✔️ Finish raffle', callback_data='finish')],
+        [InlineKeyboardButton('❌ Cancel', callback_data='cancel')]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(f'Fee set to {fee}€!', reply_markup=reply_markup)
+
+    return 'finish_setup'
+
+
+async def finish_setup(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Union[str, None]:
+    # print('finish_setup: ' + str(update))
     query = update.callback_query
 
-    print(update)
+    if query.data == 'cancel':
+        await query.message.edit_text('Cancelled! ❌')
+        context.user_data.clear()
+        return ConversationHandler.END
 
-    await query.answer()
+    chat_title = context.user_data['raffle_chat_title']
+    chat_id = context.user_data['raffle_chat_id']
+    start_date = context.user_data['raffle_start_date'] + ':00'
+    end_date = context.user_data['raffle_end_date'] + ':00'
+    entry_fee = context.user_data['raffle_entry_fee']
+    # print(chat_id, start_date, end_date, entry_fee)
 
-    if query.data == 'cancel_update':
-        await query.message.edit_text('Cancelled!')
-    else:
-        chat_id = query.data[0]
-        chat_title = query.data[1]
-        file = await context.bot.get_file(query.data[2])
+    cur.execute(f'''INSERT OR REPLACE INTO raffle(chat_id, start_date, end_date, entry_fee)
+                    VALUES ({chat_id}, "{start_date}", "{end_date}", {entry_fee})''')
+    con.commit()
 
-        if not os.path.exists(f'data/{chat_id}'):
-            os.mkdir(f'data/{chat_id}')
+    await query.message.edit_text(f'New raffle setup in {chat_title}! ✔️')
 
-        with open(f'data/{chat_id}/data.xlsx', 'wb') as f:
-            await file.download(out=f)
+    return ConversationHandler.END
 
-        await query.message.edit_text(f'Updated data in {chat_title}!')
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data.clear()
+    await update.message.reply_text('Cancelled! ❌')
+    return ConversationHandler.END
+
+
+async def timeout(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
+    # print('timeout: ' + str(update))
+    query = update.callback_query
+    await query.message.edit_text('Timed out! 🕐')
 
 
 async def graph(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -129,21 +330,38 @@ async def graph(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
     graph_path = base_path + 'graph.png'
 
     try:
+        _, start_date, end_date, entry_fee = (
+            cur.execute(
+                f'SELECT * FROM raffle WHERE chat_id = {chat_id}')
+            .fetchone())
+
         df = pd.read_excel(excel_path, usecols='A,D', header=None, names=[
             'date', 'amount'], parse_dates=True)
         df.set_index('date', inplace=True)
         df = df.iloc[::-1]
+
         df['amount'] = df['amount'].cumsum()
+        df['amount'].plot(style='xr')
 
-        df['amount'].plot()
+        # linregress
 
-        plt.title(f'{chat_title} stonks')
-        plt.xlabel(None)
-        plt.ylabel('Amount (€)')
+        total = df['amount'].max()
+        plt.title(f'{chat_title} stonks, Pool {total}€')
+        plt.xlabel('Time')
+        plt.ylabel('Pool (€)')
+        plt.grid(True, which='minor')
+        plt.legend(['Data'])
+
         plt.savefig(graph_path)
 
         with open(graph_path, 'rb') as f:
             await update.message.reply_photo(photo=f)
+
+    except sqlite3.Error as e:
+        print(e)
+        await update.message.reply_text('Error getting raffle data from database!\n\n' +
+                                        'Perhaps one is not setup yet for this chat? 🤔')
+
     except FileNotFoundError:
         await update.message.reply_text(f'No data found for {chat_title}!')
 
@@ -161,9 +379,9 @@ async def bot_added(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         username = update.effective_user.username
 
         try:
-            cur.execute(f'''INSERT OR IGNORE INTO chats(chat_id, title, admin)
+            cur.execute(f'''INSERT OR IGNORE INTO chat(chat_id, title, admin)
                             VALUES ({chat_id}, "{title}", {user_id})''')
-            cur.execute(f'''INSERT OR IGNORE INTO users(user_id, username)
+            cur.execute(f'''INSERT OR IGNORE INTO user(user_id, username)
                             VALUES ({user_id}, '{username}')''')
             cur.execute(f'''INSERT OR IGNORE INTO in_chat(user_id, chat_id)
                             VALUES ({user_id}, {chat_id})''')
@@ -187,31 +405,89 @@ async def winner(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
     if len(ent) != 2 or ent[1].type != MessageEntityType.MENTION:
         await update.message.reply_text('Please use the format /winner @username')
 
-        return
+    else:
 
-    _winner = update.message.text.split(" ")[1]
+        _username = update.message.text.split(" ")[1]
 
-    print('winner:' + str(update))
+        print('winner:' + str(update))
+
+
+async def chat_only(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('This command is not usable in private messages!')
 
 
 async def tester(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None:
     print('all handler: ' + str(update))
-    print()
+
 
 # -- MAIN --
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(BOT_TOKEN).arbitrary_callback_data(True).build()
+def main() -> None:
+    persistence = PicklePersistence(filepath='data/.pkl')
+    app = (
+        ApplicationBuilder()
+        .token(BOT_TOKEN)
+        .persistence(persistence)
+        .arbitrary_callback_data(True)
+        .build()
+    )
 
     app.add_handler(CommandHandler('start', start))
-    app.add_handler(CommandHandler(['moro', 'hello'], hello))
-    app.add_handler(MessageHandler(Document.MimeType(
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'), excel_file))
-    app.add_handler(CallbackQueryHandler(update_chat))
+
+    # added to channel
+    app.add_handler(ChatMemberHandler(bot_added, -1))
+
+    # base commands
+    app.add_handler(CommandHandler(
+        ['moro', 'hello'], hello, ~Filters.ChatType.PRIVATE))
     app.add_handler(CommandHandler(
         ['kuvaaja', 'graph'], graph, ~Filters.ChatType.PRIVATE))
-    app.add_handler(ChatMemberHandler(bot_added, -1))
     app.add_handler(CommandHandler(
         ['voittaja', 'winner'], winner, ~Filters.ChatType.PRIVATE))
-    app.add_handler(MessageHandler(Filters.ALL | Filters.Sticker.ALL, tester))
+
+    # warning about using a command in a private chat
+    app.add_handler(CommandHandler(
+        ['moro', 'hello', 'kuvaaja', 'graph', 'voittaja', 'winner'],
+        chat_only, Filters.ChatType.PRIVATE))
+
+    # YYYY-MM-DD HH:mm regex
+    two_line_date_filter = Filters.Regex(
+        r'^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-2])\s([0-1]\d|2[0-3]):([0-5]\d)\n' +
+        r'(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-2])\s([0-1]\d|2[0-3]):([0-5]\d)$')
+
+    price_filter = Filters.Regex(r'^(([1-9]\d|[1-9])(.\d)?)|(0.[1-9]\d?)$')
+
+    # sending excel file in private chat
+    app.add_handler(MessageHandler(Document.MimeType(EXCEL_MIME) &
+                                   Filters.ChatType.PRIVATE, excel_file))
+
+    # start conversation after selecting channel from excel file
+    app.add_handler(ConversationHandler(
+        entry_points=[CallbackQueryHandler(setup_raffle)],
+        states={
+            'ask_date': [CallbackQueryHandler(ask_date)],
+            'get_date': [
+                MessageHandler(two_line_date_filter, get_date),
+                MessageHandler(Filters.ALL & ~Filters.COMMAND, invalid_date),
+                CommandHandler('cancel', cancel)
+            ],
+            'ask_fee': [CallbackQueryHandler(ask_fee)],
+            'get_fee': [
+                MessageHandler(price_filter, get_fee),
+                MessageHandler(Filters.ALL & ~Filters.COMMAND, invalid_fee),
+                CommandHandler('cancel', cancel)
+            ],
+            'finish_setup': [CallbackQueryHandler(finish_setup)],
+            ConversationHandler.TIMEOUT: [CallbackQueryHandler(timeout)]
+        },
+        fallbacks=[CallbackQueryHandler(setup_raffle)],
+        conversation_timeout=60
+    ))
+
+    # test handler for all messages
+    # app.add_handler(MessageHandler(Filters.ALL | Filters.Sticker.ALL, tester))
 
     app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ con = sqlite3.connect('data/.db')
 cur = con.cursor()
 
 try:
-    cur.execute('''CREATE TABLE chats(
+    cur.execute('''CREATE TABLE chat(
                     chat_id INTEGER PRIMARY KEY,
                     title TEXT,
                     admin INTEGER,
@@ -12,7 +12,7 @@ try:
                     cur_winner INTEGER
                   )''')
 
-    cur.execute('''CREATE TABLE users(
+    cur.execute('''CREATE TABLE user(
                     user_id INTEGER PRIMARY KEY,
                     username TEXT
                   )''')
@@ -22,6 +22,14 @@ try:
                     chat_id INTEGER REFERENCES chats(chat_id),
                     PRIMARY KEY (user_id, chat_id)
                   )''')
+
+    cur.execute('''CREATE TABLE raffle(
+                    chat_id INTEGER PRIMARY KEY REFERENCES chats(chat_id),
+                    start_date TEXT CHECK (start_date IS datetime(start_date)),
+                    end_date TEXT CHECK (end_date IS datetime(end_date)),
+                    entry_fee INTEGER
+                  )''')
+
     con.commit()
 except sqlite3.OperationalError as e:
     print('Tables already setup!')
